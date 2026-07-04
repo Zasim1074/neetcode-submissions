156bb3101c43800d2f210a70979d2a43512bb7bf
Jaseem Quraishi <rdgvszasim@gmail.com>
2026-07-04 12:50:53 +0530

    Add: three-integer-sum - submission-8

---
+++ Data Structures & Algorithms/three-integer-sum/submission-8.js
@@ -0,0 +1,31 @@
+class Solution {
+    threeSum(nums) {
+        const sarr = nums.sort((a, b) => a - b);
+        const result = [];
+
+        for (let i = 0; i < sarr.length - 1; i++) {
+            if (i > 0 && sarr[i] === sarr[i - 1]) continue;
+
+            let left = i + 1,
+                right = sarr.length - 1;
+
+            while (left < right) {
+                const sum = sarr[i] + sarr[left] + sarr[right];
+
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    result.push([sarr[i], sarr[left], sarr[right]]);
+                    left++;
+                    right--;
+
+                    while (left < right && sarr[left] === sarr[left - 1]) left++;
+                    while (left < right && sarr[right] === sarr[right + 1]) right--;
+                }
+            }
+        }
+        return result;
+    }
+}
